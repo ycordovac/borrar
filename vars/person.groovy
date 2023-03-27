62b49b06ad90265c6a99e50857fc006f903cc282
ycordovac <117431970+ycordovac@users.noreply.github.com>
2023-03-27 12:03:38 +0200

    convertir en libreria Orientada a Objeto

--- vars/person.groovy
+++ vars/person.groovy
@@ -1,7 +1,7 @@
 import org.foot.Person
 
 def crearPerson(String nombre, String apellidos) {
-  println(nom)
+  println(nombre)
   p=new Person(nombre: nombre, apellidos: apellidos)
   println(p)
   returnValue p 
